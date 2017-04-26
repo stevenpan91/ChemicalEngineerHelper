5ee291a6939789705a0b3422acbe53ee6db66afa
acmujica <amujica7564@gmail.com>
2017-04-27 00:15:58 +0800

    Pretty MainScreen

--- MainScreen.js
+++ MainScreen.js
@@ -12,6 +12,7 @@ import {
   Navigator
 } from 'react-native';
 
+import Logo from './app/components/Logo/Logo';
 
 class MainScreen extends Component {
   //static navigationOptions = { title: 'Welcome', };
@@ -22,18 +23,29 @@ class MainScreen extends Component {
             renderScene={this.renderScene.bind(this)}
             navigator={this.props.navigator}
             navigationBar={
-              <Navigator.NavigationBar style={{backgroundColor: '#246dd5'}}
+              <Navigator.NavigationBar style={styles.navBar}
                   routeMapper={NavigationBarRouteMapper} />
             } />
       );
   }
     renderScene(route, navigator) {
       return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}} >
-          <TouchableHighlight style={{backgroundColor: 'yellow', padding: 10}}
-              onPress={this.gotoEquations.bind(this)}>
-            <Text style={{backgrondColor: 'yellow', color: 'green'}}>To Equations</Text>
-          </TouchableHighlight>
+        <View style={styles.container}>
+          <View style={styles.main}>
+            <Logo style={styles.image}/>
+            <Text style={styles.welcome}> Welcome to the Chemical Engineer Helper App!</Text>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={this.gotoEquations.bind(this)}>
+                <Text style={styles.button}>Get Started!</Text>
+              </TouchableOpacity>
+              <Text> </Text>
+              <TouchableOpacity onPress={this.gotoEquations.bind(this)}>
+                <Text style={styles.button}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.quote}> "Quote of Deep thought" </Text>
+            <Text style={styles.quote}> - Scientist , 1968 </Text>
+          </View>
         </View>
       );
   }
@@ -46,9 +58,9 @@ class MainScreen extends Component {
 var NavigationBarRouteMapper = {
   LeftButton(route, navigator, index, navState) {
     return (
-      <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}
+      <TouchableOpacity style={styles.main}
           onPress={() => navigator.parentNavigator.pop()}>
-        <Text style={{color: 'white', margin: 10,}}>
+        <Text style={styles.navTitle}>
           Back
         </Text>
       </TouchableOpacity>
@@ -59,13 +71,65 @@ var NavigationBarRouteMapper = {
   },
   Title(route, navigator, index, navState) {
     return (
-      <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}>
-        <Text style={{color: 'white', margin: 10, fontSize: 16}}>
+      <TouchableOpacity style={styles.main}>
+        <Text style={styles.navTitle}>
           Main Page
         </Text>
       </TouchableOpacity>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#03D6F3',
+    paddingTop : 40,
+  },
+  main: {
+    flex: .9,
+    justifyContent:'center',
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  button: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    backgroundColor: '#033BE5',
+    marginBottom: 5,
+    padding: 12,
+    overflow: 'hidden',
+    borderRadius: 6,
+  },
+  quote : {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontFamily: 'snell roundhand',
+  },
+  image : {
+    tintColor: '#ffffff',
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  navBar: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#246dd5',
+    paddingTop : 40,
+  },
+  navTitle: {
+    color: 'white',
+    margin: 10,
+    fontSize: 16,
+  },
+});
 
 module.exports=MainScreen;
