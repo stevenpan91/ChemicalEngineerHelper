92eb64a12aa88d544e8d0bf08888560f7ca7d9d9
steven <stevenpan91@gmail.com>
2017-09-29 06:42:04 +0900

    Added Reynolds

--- app/equations/ReynoldsNumber.js
+++ app/equations/ReynoldsNumber.js
@@ -7,6 +7,7 @@ import {
   StyleSheet, 
   TouchableOpacity } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import CalculationClass from '../classes/CalculationClass'
 
 export default class ReynoldsNumber extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -17,13 +18,41 @@ export default class ReynoldsNumber extends Component {
       title="Settings" onPress={()=>navigate('Settings')}/>,
     }
   };
+
+  calcReynolds = function(state,updateResult) {
+     var d = parseFloat(state.cLines[0].SIInput)
+     var mfr = parseFloat(state.cLines[1].SIInput)
+     var pid = parseFloat(state.cLines[2].SIInput)
+     var visc = parseFloat(state.cLines[3].SIInput)
+
+     var vel = mfr/d/(Math.PI*Math.pow(pid,2)/4)
+
+     updateResult(vel)
+  }
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        density: '',
+        mass_flow_rate: '',
+        pipe_id: '',
+        viscosity:'',
+        Re:'N/A'
+      };
+
+    }
+
   render(){
     const { navigate } = this.props.navigation;
     const { params } = this.props.navigation.state;
     return (
-      <View style={{flex:1, backgroundColor: '#03D6F3'}}>
-        <Text style={styles.hard}>Reynolds Number (Diameter)</Text>
-      </View>
+      <CalculationClass varLabels={["Density","Mass Flow", "Pipe ID", "Viscosity"]}
+                                calcVals={[this.state.density,this.state.mass_flow_rate,
+                                           this.state.pipe_id,this.state.viscosity]}
+                                unitSets={["M/L3","M/Z2","L","P*Z"]}
+                                resultUnitSet={""}
+                                //calcResult={this.state.density}
+                                calcFunction = {this.calcReynolds.bind(this)}/>
     )
   };
 }
